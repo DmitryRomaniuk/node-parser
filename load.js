cd3f1fdb43ee01cb6b1c956a6d539d74d8b7061d
DmitryRomaniuk <Kanumowa@gmail.com>
2016-09-25 20:38:44 +0300

    Correct files

--- load.js
+++ load.js
@@ -8,9 +8,12 @@ let promise = new Promise(function (resolve) {
     fs.readFile('radio.txt', 'utf8', (err, data) => {
         if (err) throw err;
         arrRadio = data.split(",");
-        //console.log(arrRadio);
+        arrRadio = arrRadio.map( function (val, index) {
+           return val.match(/http\:\/\/[0-9a-zA-Z.:-]{1,}/gi)[0];
+        })
         resolve(arrRadio);
-        console.log('https://developer.mozilla.org05646'.match(/http.+?/gi));
+        //console.log('http://us2.internet-radio.com:8137/listen.pls'.match(/http\:\/\/[0-9a-zA-Z.:-]{1,}/gi)[0]);
+        //console.log(arrRadio);
         /*callNextStep({
         hostname: arrRadio[0].match(/http:*+?(d+)/gi),
         port: 80,
@@ -23,71 +26,66 @@ let promise = new Promise(function (resolve) {
         });*/
     })
 })
+.then(function (val) {
+    function nextUrl(a) {
+        console.log(arrRadio[a]);
+        var url = arrRadio[a].match(/http:[^:]+/gi)[0];
+        console.log(url);
+        var port = arrRadio[a].match(/[^\d:]\d+/gi)[0];
+        console.log(port);
+        var options = {
+        hostname: url,
+        port: 80,
+        path: '/listen.pls',
+        method: 'GET'
+        };
+        console.log(arrRadio[a]);
+        var req = http.request(options, (res) => {
+        //console.log(`STATUS: ${res.statusCode}`);
+        //console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+        res.setEncoding('utf8');
+        res.on('data', (chunk) => {
+                let bodyFile = chunk.toString();
+                let nowTime = Date.now();
+                nowTime = nowTime + ".pls";
+                fs.writeFile(nowTime, bodyFile, 'utf8', (err) => {
+                    if (err) throw err;
+                    //console.log(`It\'s saved!`);
+                    });
+        });
+        res.on('end');
+        });
+        req.on('error', (e) => {
+        console.log(`problem with request: ${e.message}`);
+        });
+
+        // write data to request body
+        //req.write(postData);
+        req.end(() => {
+            console.log(arrRadio[a]);
+            i++;
+            if (i<arrRadio.length) {promise.then(()=>{nextUrl(i)})}
+        });
+    }
+    nextUrl(i);
+})
+
+
+
 /*
-    .then(function (val) {
-        for (i=0; i < (arrRadio.length-1)/2; i++) {
-            let a = i;
-                http.get(val[a], (res) => {
-                    //console.log(`Got response: ${res.statusCode}`);
-                    res.on('data', (text) => {
-                        let bodyFile = text.toString();
-                        let nowTime = Date.now();
-                        nowTime = nowTime + ".pls";
-                        fs.writeFile(nowTime, bodyFile, 'utf8', (err) => {
-                            if (err) throw err;
-                        });
-                    })
-                    console.log(a + 'connection closed')
-                    res.resume();
-                }).on('error', (e) => {
-                    console.log(`Got error: ${e.message}`);
-                });
-        }
-    })
-*/
-//http.on('end',()=>console.log('connection closed'))
-
-
-
-var postData = JSON.stringify({
-  'msg' : 'Hello World!'
-});
-
-var options = {
-  hostname: 'www.google.com',
-  port: 80,
-  path: '/upload',
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Content-Length': Buffer.byteLength(postData)
-  }
-};
-function callNextStep(options) {
-var req = http.request(options, (res) => {
-  //console.log(`STATUS: ${res.statusCode}`);
-  //console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-  res.setEncoding('utf8');
-  res.on('data', (chunk) => {
-        let bodyFile = chunk.toString();
-        let nowTime = Date.now();
-        nowTime = nowTime + ".pls";
-        fs.writeFile(nowTime, bodyFile, 'utf8', (err) => {
-            if (err) throw err;
-            //console.log('It\'s saved!');
+            http.get(val[a], (res) => {
+                //console.log(`Got response: ${res.statusCode}`);
+                res.on('data', (text) => {
+                    let bodyFile = text.toString();
+                    let nowTime = Date.now();
+                    nowTime = nowTime + ".pls";
+                    fs.writeFile(nowTime, bodyFile, 'utf8', (err) => {
+                        if (err) throw err;
+                    });
+                })
+                console.log(a + 'connection closed')
+                res.resume();
+            }).on('error', (e) => {
+                console.log(`Got error: ${e.message}`);
             });
-  });
-  res.on('end');
-});
-
-req.on('error', (e) => {
-  console.log(`problem with request: ${e.message}`);
-});
-
-// write data to request body
-req.write(postData);
-req.end(() => {
-    //if () {console.log('No more data in response.');}
-  });
-}
-
+*/
